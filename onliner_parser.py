b6db6d22e36eae0a204ac9e8181bb84295f60b03
Dzianis Haurylavets <dzianis.haurylavets-ext@ax-ao.de>
2019-07-16 21:46:24 +0300

    Parse main phones' attributes

--- onliner_parser.py
+++ onliner_parser.py
@@ -1,4 +1,8 @@
 import re
+import urllib
+
+import pandas as pd
+from pyquery import PyQuery as pq
 
 
 def fetch_links(html):
@@ -10,3 +14,78 @@ def fetch_links(html):
 
     return links
 
+
+def fetch_products(links):
+    df = pd.DataFrame()
+    for index, link in enumerate(links):
+        html = read_page(link)
+        df = parse_page(link, html, df)
+        print("{:.2f}% parsed".format((index+1) * 100 / len(links)))
+    return df
+
+
+def read_page(address):
+    fp = urllib.request.urlopen(address)
+    mybytes = fp.read()
+    fp.close()
+    return mybytes.decode("utf8")
+
+
+h1_pattern = re.compile(r'<h1 class="catalog-masthead__title" itemprop="name">\n *(.+?) *\n')
+shirt_descr_pattern = re.compile(r'<p itemprop="description"> *(.+?) *</p>')
+title_pattern = re.compile(r'<title>(.+?)</title>')
+img_pattern = re.compile(r'<img.+id="device-header-image".+>')
+img_url_pattern = re.compile(r'.*src="(.*?)".*')
+img_title_pattern = re.compile(r'.*title="(.*?)".*')
+img_alt_pattern = re.compile(r'.*alt="(.*?)".*')
+descr_in_content_pattern = re.compile(r'<div class="product-specs__table-small i-faux-td">[\n\r]{1,2} *<p>(.*)</p>')
+low_prices_pattern = re.compile(r'itemprop="lowPrice">(.*)<')
+high_prices_pattern = re.compile(r'itemprop="highPrice">(.*)<')
+
+
+def parse_page(url, html, df):
+    return df.append({
+        **parse_main_attributes(url, html),
+        **parse_switchers(html)
+    }, ignore_index=True)
+
+
+def parse_main_attributes(url, html):
+    def apply_pattern(pattern, html=html):
+        res = pattern.findall(html)
+        if res is None or len(res) == 0:
+            return ' '
+        return res[0]
+
+    img_html = apply_pattern(img_pattern)
+
+    return {
+        'iPhone URL': url,
+        'Title': apply_pattern(title_pattern),
+        'Description': apply_pattern(shirt_descr_pattern),
+        'H1': apply_pattern(h1_pattern),
+        'Img URL': apply_pattern(img_url_pattern, img_html),
+        'Img Title': apply_pattern(img_title_pattern, img_html),
+        'Img Alt': apply_pattern(img_alt_pattern, img_html),
+        'Code review': 'TODO: Грузится динамически',
+        'Description in content': apply_pattern(descr_in_content_pattern),
+        'Prices': apply_pattern(low_prices_pattern) + ' - ' + apply_pattern(high_prices_pattern),
+    }
+
+
+def parse_switchers(html):
+    switchers = pq(html)('.offers-description-filter__row')
+    rows = {}
+    for switch in switchers:
+        switch = pq(switch)
+        name = switch('.offers-description-filter__sign').text()
+        links = []
+
+        for a in switch('a.offers-description-filter-control'):
+            a = pq(a)
+            links.append(a.attr('href'))
+
+        links = '\n'.join(links)
+        rows[name] = links
+
+    return rows
--- runner.py
+++ runner.py
@@ -1,4 +1,7 @@
+import pandas as pd
+
 from onliner_parser import fetch_links
+from onliner_parser import fetch_products
 
 file = open("parseme.html", "r", encoding="utf8")
 html = file.read()
@@ -6,5 +9,12 @@ file.close()
 
 links = fetch_links(html)
 
+writer = pd.ExcelWriter('out/iphones.xlsx')
+
 print(*links, sep='\n', file=open("out/links.txt", "w"))
 print(links.__len__(), 'links found')
+
+df = fetch_products(links)
+df.to_excel(writer, sheet_name='parsed iphones')
+
+writer.close()
